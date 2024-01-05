3914dff62a52afa7790e95f6af285ded0e588618
Paulo Calueto <paulo.calueto.francisco@gmail.com>
2024-01-05 07:48:24 +0100

    counter livewire

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,5 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="container mx-auto">
+    <button class="bg-blue-600 px-4 py-2 text-white rounded" wire:click="increment">+</button>
+        <span>{{$counter}}</span>
+    <button class="bg-blue-600 px-4 py-2 text-white" wire:click="decrement">-</button>
 </div>
